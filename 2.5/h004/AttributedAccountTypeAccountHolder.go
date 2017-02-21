9264c26659a666cbd3650bba9b2a696dafeace14
Thomas Kuchs <thomas.kuchs@xcom.de>
2017-02-21 15:12:00 +0100

    Print goxc version/rev. in complex elements

--- 2.5/h004/AttributedAccountTypeAccountHolder.go
+++ 2.5/h004/AttributedAccountTypeAccountHolder.go
@@ -1,4 +1,4 @@
-// Generated with goxc v - rev
+// Generated with goxc v0.1.2 - rev bbe25b23ba83e8f8464e681ca3e514eee51fd2ba
 package h004
 
 import w3c "github.com/openyard/ebics/2.5/w3c"
--- 2.5/h004/HPBResponseOrderDataType.go
+++ 2.5/h004/HPBResponseOrderDataType.go
@@ -1,4 +1,4 @@
-// Generated with goxc v0.1.1 - rev bae2cf01854d664b985cae6986076979716034c7
+// Generated with goxc v0.1.2 - rev bbe25b23ba83e8f8464e681ca3e514eee51fd2ba
 package h004
 
 import esig "github.com/openyard/ebics/2.5/s001"
